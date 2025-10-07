79849702e55c0d7dc99ae17c02ea2ef64124219d
21849 <qijiewu666@gmail.com>
2025-10-07 16:50:08 -0400

    updates flying crow

--- Source/DMS332/Private/Pawns/Bird.cpp
+++ Source/DMS332/Private/Pawns/Bird.cpp
@@ -1,31 +1,95 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "Pawns/Bird.h"
 #include "Components/CapsuleComponent.h"
 #include "Components/SkeletalMeshComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
 
-// Sets default values
-ABird::ABird() {
-  // Set this pawn to call Tick() every frame.  You can turn this off to improve
-  // performance if you don't need it.
-  PrimaryActorTick.bCanEverTick = true;
-  // this creates the Capsule based on UCapsuleComponent template
-  Capsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule"));
-  // and sets it as the root component
-  SetRootComponent(Capsule);
-  // again, take the pointer from Bird.h and create the skeletal mesh
-  BirdMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("BirdMesh"));
-  // attach it to the Root Component so they all move together
-  BirdMesh->SetupAttachment(GetRootComponent());
+// input system headers
+#include "Components/InputComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputActionValue.h"
+#include "GameFramework/FloatingPawnMovement.h"
+
+ABird::ABird()
+{
+    PrimaryActorTick.bCanEverTick = true;
+
+    Capsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule"));
+    Capsule->SetCapsuleHalfHeight(20.f);
+    Capsule->SetCapsuleRadius(15.f);
+    SetRootComponent(Capsule);
+
+    MovementComponent = CreateDefaultSubobject<UFloatingPawnMovement>(TEXT("MovementComponent"));
+    MovementComponent->UpdatedComponent = Capsule;
+
+    BirdMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("BirdMesh"));
+    BirdMesh->SetupAttachment(GetRootComponent());
+
+    SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+    SpringArm->SetupAttachment(Capsule);
+    SpringArm->TargetArmLength = 300.f;
+    SpringArm->bUsePawnControlRotation = true;
+
+    ViewCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("ViewCamera"));
+    ViewCamera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
+    ViewCamera->bUsePawnControlRotation = false;
+
+    AutoPossessPlayer = EAutoReceiveInput::Player0;
 }
 
-// Called when the game starts or when spawned
-void ABird::BeginPlay() { Super::BeginPlay(); }
+void ABird::BeginPlay()
+{
+    Super::BeginPlay();
 
-// Called every frame
-void ABird::Tick(float DeltaTime) { Super::Tick(DeltaTime); }
+    if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+    {
+        if (UEnhancedInputLocalPlayerSubsystem* Subsystem =
+            ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(
+                PlayerController->GetLocalPlayer()))
+        {
+            Subsystem->AddMappingContext(BirdMappingContext, 0);
+        }
 
-// Called to bind functionality to input
-void ABird::SetupPlayerInputComponent(UInputComponent *PlayerInputComponent) {
-  Super::SetupPlayerInputComponent(PlayerInputComponent);
+        bUseControllerRotationPitch = true;
+        bUseControllerRotationYaw = true;
+    }
+}
+
+void ABird::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+}
+
+void ABird::Move(const FInputActionValue& Value)
+{
+    const float DirectionValue = Value.Get<float>();
+    if (Controller && DirectionValue != 0.f)
+    {
+        AddMovementInput(GetActorForwardVector(), DirectionValue);
+    }
+}
+
+void ABird::Look(const FInputActionValue& Value)
+{
+    const FVector2D LookAxisValue = Value.Get<FVector2D>();
+    if (Controller)
+    {
+        AddControllerYawInput(LookAxisValue.X);
+        AddControllerPitchInput(-LookAxisValue.Y);
+    }
+}
+
+void ABird::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+    if (UEnhancedInputComponent* EnhancedInputComponent =
+        CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+    {
+        EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered,
+            this, &ABird::Move);
+        EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered,
+            this, &ABird::Look);
+    }
 }
